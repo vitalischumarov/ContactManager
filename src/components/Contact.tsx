8dcfbffb68d605d501f6beddb9a653e86efa24da
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-30 09:53:53 +0200

    contact view done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,8 +4,28 @@ import "./Contact.scss";
 export default function Contact() {
   return (
     <>
-      <Link to={"/editView"}>
-        <div className="Contact"></div>
+      <Link to={"/editView"} className="link">
+        <div className="Contact">
+          <div className="imge">image</div>
+          <div className="details">
+            <div className="details__name">
+              <h3>Vitali</h3>
+            </div>
+            <div className="details__contact">
+              <div className="leftDetails">
+                <span>01.03.1988</span>
+                <span>Male</span>
+                <span>test@gmail.com</span>
+              </div>
+              <div className="rightDetails">
+                <span>Musterstrasse 12</span>
+                <span>123456789</span>
+                <span>vitali-schumarov.ch</span>
+              </div>
+            </div>
+          </div>
+          <div className="btn">+</div>
+        </div>
       </Link>
     </>
   );
