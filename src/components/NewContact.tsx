e9d12e422bffe56c3cf6b233170d1a0aa898bfba
Vitali Schumarov <v.schumarov@icloud.com>
2025-03-30 13:35:46 +0200

    form done

--- src/components/NewContact.tsx
+++ src/components/NewContact.tsx
@@ -3,25 +3,27 @@ import "./NewContact.scss";
 export default function NewContact() {
   return (
     <div className="NewContact">
-      <label>Username</label>
-      <input type="text" name="" id="" className="input" />
-      <label>Geburtsdatum</label>
-      <input type="date" name="" id="" className="input" />
-      <label htmlFor={"geschlecht"}>Geschlecht:</label>
-      <select name="geschlecht" id="geschlecht">
-        <option value={"männlich"}>Männlich</option>
-        <option value={"weiblich"}>Weiblich</option>
-      </select>
-      <label>Email Adresse</label>
-      <input type="email" name="" id="" className="input" />
-      <label>Post Adresse</label>
-      <input type="text" name="" id="" className="input" />
-      <label>Telefonnummer</label>
-      <input type="text" name="" id="" className="input" />
-      <label>Webseite</label>
-      <input type="text" name="" id="" className="input" />
-      <br />
-      <button>Submit</button>
+      <form action="" className="Form">
+        <label>Username</label>
+        <input type="text" name="" id="" className="input" />
+        <label>Geburtsdatum</label>
+        <input type="date" name="" id="" className="input" />
+        <label htmlFor={"geschlecht"}>Geschlecht:</label>
+        <select name="geschlecht" id="geschlecht">
+          <option value={"männlich"}>Männlich</option>
+          <option value={"weiblich"}>Weiblich</option>
+        </select>
+        <label>Email Adresse</label>
+        <input type="email" name="" id="" className="input" />
+        <label>Post Adresse</label>
+        <input type="text" name="" id="" className="input" />
+        <label>Telefonnummer</label>
+        <input type="text" name="" id="" className="input" />
+        <label>Webseite</label>
+        <input type="text" name="" id="" className="input" />
+        <br />
+        <button className="Button">Submit</button>
+      </form>
     </div>
   );
 }
