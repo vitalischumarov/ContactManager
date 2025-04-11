72da8ca313ca2c4c38bb8e65667b9d32802eb6a1
Vitali Schumarov <v.schumarov@icloud.com>
2025-04-11 05:58:28 +0200

    contact data are available in edit Component

--- src/components/Edit.tsx
+++ src/components/Edit.tsx
@@ -1,8 +1,43 @@
 import { useParams } from "react-router-dom";
+import { useContext, useState } from "react";
+import { PersonContext } from "../App";
+import { iPerson } from "../Model/Contact";
 
 function Edit() {
+  const context = useContext(PersonContext);
   const { itemID } = useParams();
-  return <h3>{itemID}</h3>;
+  const id = checkDataType(itemID);
+  const [person, setPerson] = useState(searchForContact(id));
+
+  function checkDataType(itemID: string | undefined) {
+    if (itemID !== undefined) {
+      return Number(itemID);
+    } else {
+      return 0;
+    }
+  }
+
+  function searchForContact(searchedID: number): iPerson {
+    const searchedContact = context?.contacts.find(
+      (contact) => contact.id === searchedID
+    );
+    if (searchedContact !== undefined) {
+      return searchedContact;
+    } else {
+      return {
+        id: Math.random(),
+        vorname: "",
+        geburtstag: "",
+        geschlecht: "",
+        email: "",
+        strasse: "",
+        telefon: "",
+        webseite: "",
+      };
+    }
+  }
+  console.table(person);
+  return <h3>{id}</h3>;
 }
 
 export default Edit;
